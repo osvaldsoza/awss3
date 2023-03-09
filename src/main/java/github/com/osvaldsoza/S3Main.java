7a869d41756b59c266f833983ff0bddc9beb704f
Osvaldo Souza Ferreira <osvald.soza@gmail.com>
2023-03-09 10:16:47 -0300

    Solved erro

--- src/main/java/github/com/osvaldsoza/S3Main.java
+++ src/main/java/github/com/osvaldsoza/S3Main.java
@@ -3,6 +3,6 @@ package github.com.osvaldsoza;
 public class S3Main {
 
     public static void main(String[] args) {
-        OperationsAwsS3.manipularBucket();
+        OperationsAwsS3.handleBucket();
     }
 }
