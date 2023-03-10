123d1657445387356e94833652fe17201b3d45b4
Osvaldo Souza Ferreira <osvald.soza@gmail.com>
2023-03-10 15:25:08 -0300

    update code

--- src/main/java/github/com/osvaldsoza/Credentials.java
+++ src/main/java/github/com/osvaldsoza/Credentials.java
@@ -2,6 +2,6 @@ package github.com.osvaldsoza;
 
 public class Credentials {
 
-    public static final String ACCESS_KEY = "AKIAZFJ7BIS7UR22AE4V";
-    public static final String SECRET_KEY = "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";
+    public static final String ACCESS_KEY = "AKIAZFJ7BIS7SZHBP3G6";
+    public static final String SECRET_KEY = "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";
 }
